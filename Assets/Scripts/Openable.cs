8fc85469ccc542327549c03d08cc6e240186db74
Kedar Chandulur <47148900+KedarChandulur@users.noreply.github.com>
2024-05-27 16:45:24 -0500

    First commit
    Main scene and Openable Script added.

---
+++ Assets/Scripts/Openable.cs
@@ -0,0 +1,80 @@
+using System.Collections;
+using UnityEngine;
+
+public abstract class Openable : MonoBehaviour
+{
+    private Animator anim;
+    private Transform player;
+    private float interactableDistance;
+    private bool isOpen;
+
+    protected void Setup(float playerInteractableDistance)
+    {
+        GameObject playerGO_Ref = GameObject.FindWithTag("Player");
+
+        if (playerGO_Ref == null)
+        {
+            Debug.LogError("Couldn't find the player reference on application start.");
+
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.ExitPlaymode();
+#endif
+            return;
+        }
+
+        player = playerGO_Ref.transform;
+
+        if (!this.transform.TryGetComponent<Animator>(out anim))
+        {
+            if (!this.transform.parent.TryGetComponent<Animator>(out anim))
+            {
+                Debug.LogError("Couldn't find the animation reference on application start.");
+
+#if UNITY_EDITOR
+                UnityEditor.EditorApplication.ExitPlaymode();
+#endif
+
+                return;
+            }
+        }
+
+        interactableDistance = playerInteractableDistance;
+        isOpen = false;
+    }
+
+    void OnMouseOver()
+    {
+        if (!player)
+        {
+            Debug.LogError("Lost reference to player");
+            return;
+        }
+
+        float dist = Vector3.Distance(player.position, this.transform.position);
+
+        if (dist < interactableDistance)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                if (isOpen)
+                {
+                    StartCoroutine(CloseAnim());
+                }
+                else
+                {
+                    StartCoroutine(OpenAnim());
+                }
+
+                isOpen = !isOpen;
+            }
+        }
+    }
+
+    protected void PlayAnimation(string name)
+    {
+        anim.Play(name);
+    }
+
+    protected abstract IEnumerator OpenAnim();
+    protected abstract IEnumerator CloseAnim();
+}
