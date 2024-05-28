1136486535147da6aa8d62657d6f301239ea48b4
Kedar Chandulur <47148900+KedarChandulur@users.noreply.github.com>
2024-05-28 16:45:57 -0500

    Fixed the index bug.

--- Assets/Scripts/CodeParts.cs
+++ Assets/Scripts/CodeParts.cs
@@ -4,12 +4,11 @@ using UnityEngine;
 [RequireComponent(typeof(TextMeshPro))]
 public class CodeParts : MonoBehaviour
 {
-    private static System.Collections.Generic.List<CodeParts> parts = new System.Collections.Generic.List<CodeParts>(4);
+    [SerializeField]
+    private ushort index = 0;
 
     private void Awake()
     {
-        parts.Add(this);
-
         CodeManager.OnCodeSet += OnCodeSet;
     }
 
@@ -20,8 +19,6 @@ public class CodeParts : MonoBehaviour
 
     private void OnCodeSet(string code)
     {
-        int index = parts.IndexOf(this);
-
         this.GetComponent<TextMeshPro>().text = code[index].ToString();
     }
 }
